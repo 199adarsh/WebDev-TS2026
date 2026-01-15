92019002be20d405f1e6ee5cf599d72bd970485c
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-01-15 18:30:33 +0530

    Card  Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import SyntheticHero from './components/SyntheticHero'
 import { AppleLiquidGlassNav } from './components/AppleNav'
 import { BlurFade } from "./components/ui/blur-fade"
+import { ProgressiveBlur } from "./components/progressiveblur"
+import { Skiper17 } from "./components/cardscroll"
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
-      <BlurFade>
+    <div className="min-h-screen bg-black relative">
         <AppleLiquidGlassNav />
-  
+      <ProgressiveBlur position="top" backgroundColor="rgba(0,0,0,0.3)" height="100px" blurAmount="8px" className="z-10" />
+      <BlurFade>    
+        
         <SyntheticHero 
           title="WHERE TOP ENGINERS RISE"
           description="A national-level technical event by DKTE."
         />
       </BlurFade>
+      
+      {/* Sticky Card Scroll Section */}
+      <div className="h-screen relative bg-black">
+        <Skiper17 />
+      </div>
+      
+      <ProgressiveBlur position="bottom" backgroundColor="rgba(0,0,0,0.3)" height="100px" blurAmount="8px" className="z-10" />
     </div>
   )
 }
